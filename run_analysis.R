291cca660f7d72182ce1251a18b5a52d4367b498
Zils <steven.zylinski@yahoo.fr>
2018-09-06 17:56:02 +0200

    First version of the duty

---
+++ run_analysis.R
@@ -0,0 +1,59 @@
+# Library
+
+library(dplyr)
+
+# Define Working Directory
+
+wd<-"/Users/Zils2/Downloads/UCI HAR Dataset/"
+setwd(wd)
+
+# Open file
+
+#Data
+
+features<-read.table("features.txt")
+activity<-read.table("activity_labels.txt")
+
+#Test
+X_test<-read.table("test/X_test.txt")
+y_test<-read.table("test/y_test.txt")
+subject_test<-read.table("test/subject_test.txt")
+body_acc_x_test<-read.table("test/Inertial Signals/body_acc_x_test.txt")
+
+#Train
+X_train<-read.table("train/X_train.txt")
+y_train<-read.table("train/y_train.txt")
+subject_train<-read.table("train/subject_train.txt")
+body_acc_x_train<-read.table("train/Inertial Signals/body_acc_x_train.txt")
+
+# Step 1 : Merging File
+
+X<-bind_rows(X_train,X_test)
+y<-bind_rows(y_train,y_test)
+subject<-bind_rows(subject_train,subject_test)
+df<-bind_cols(subject,X,y)
+      
+# Step 4 : Label the data with descriptive variables names
+
+names(df)<-c("subject",as.character(features[,2]),"Activity")
+
+
+# Step 2 : Keeping only measurements based on mean and standard deviation
+      
+df<-df[,c("subject",grep("[Mm]ean\\(|[Ss]td",names(df),value = TRUE),"Activity")]
+
+# Step 3 : Use descriptive activity names to name the variable Activity
+
+df<-merge(df,activity,by.x = 'Activity', by.y = 'V1', all.x = TRUE)
+df <- df %>%
+  select(-Activity) %>%
+  rename(Activity = V2)
+
+# Step 5 : Creating a tidy data with avg of each variables by activity and subject
+
+tidydf<- df %>%
+  group_by(subject,Activity) %>%
+  summarize_all(funs(mean))
+
+write.table(tidydf,file = 'tidy_data.txt',row.names = FALSE)
+
